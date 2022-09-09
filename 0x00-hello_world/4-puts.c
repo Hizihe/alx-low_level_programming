63c8a35e3b7acd324f7a3edd7c854af118b25feb
Hizihe <adeleyeadelani01@gmail.com>
2022-09-08 19:36:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   * Description: use puts() to print to standard output
+ *    * Return: Always 0 (Sucess)
+ *     * Author: Omotoye Shamsudeen Adekoya
+ *      */
+
+int main(void)
+{
+		/* String Initialization */
+		char message[] = "\"Programming is like building a multilingual puzzle";
+			/* Writing the string to stdout */
+			puts(message);
+				return (1);
+}

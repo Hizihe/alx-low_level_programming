ad2cb8c98152f5b0987f0262caec49bc6fd8efce
Hizihe <adeleyeadelani01@gmail.com>
2022-09-15 21:35:31 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,36 +1,45 @@
-#include "main.h" 
-#include <stdio.h> 
- 
-/** 
-* main - prints the numbers from 1 to 100, followed by a new line 
-* but for multiples of three prints Fizz instead of the number 
-* and for the multiples of five prints Buzz 
-* Return: Always 0 (Success) 
-*/ 
-int main(void) 
-{ 
-int i; 
- 
-for (i = 1; i <= 100; i++) 
-{ 
-if (i % 3 == 0 && i % 5 != 0) 
-{ 
-printf(" Fizz"); 
-} else if (i % 5 == 0 && i % 3 != 0) 
-{ 
-printf(" Buzz"); 
-} else if (i % 3 == 0 && i % 5 == 0) 
-{ 
-printf(" FizzBuzz"); 
-} else if (i == 1) 
-{ 
-printf("%d", i); 
-} else 
-{ 
-printf(" %d", i); 
-} 
-} 
-printf("\n"); 
- 
-return (0); 
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * @brief The "Fizz-Buzz test" is an interview question designed to help
+ *     filter out the 99.5% of programming job candidates who can’t seem
+ *     to program their way out of a wet paper bag.This is program that prints
+ *     the numbers from 1 to 100, followed by a new line.
+ * Return: (int)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+		else
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
 }

48bfc522d7f7ab72f3ddae270b7893abc44ac006
Hizihe <adeleyeadelani01@gmail.com>
2022-09-15 20:40:20 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-*nul - multiplies two numbers
-*@a: first number
-*@b: secomd number
-*Return: returns result
+* nul - multiplies two numbers
+* @a: first number
+* @b: secomd number
+* Return: returns result
 */
 
 int mul(int a, int b)

11c059f54e701d6870d5301c1cb09a4f94c9dc64
Hizihe <adeleyeadelani01@gmail.com>
2022-09-22 18:17:31 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,14 +12,28 @@
 
 char *_strcat(char *dest, char *src)
 {
+	char *_dest = dest;
+	int start_cat = 0;
 
-	int index = 0, dest_len = 0;
+	while (1)
+	{
+		if (*_dest == '\0' && start_cat == 0)
+		{
+			start_cat = 1;
+			*_dest = *src;
+			src++;
+		}
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+		else if (start_cat == 1)
+		{
+			*_dest = *src;
+			if (*src == '\0')
+			{
+				break;
+			}
+			src++;
+		}
+		_dest++;
+	}
 	return (dest);
 }
